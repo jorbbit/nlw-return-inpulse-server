6273f4d48c355014fecda1576ae601d0f3eb9e29
jorbbit <jonatas.sp@gmail.com>
2022-05-08 02:41:14 -0300

    first commit

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "9cc327556fb6a1",
+    pass: "36a82d8f9d3f5b"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Jonatas Oliveira <jonatas@orbbit.com>',
+      subject: 'Novo Feedback',
+      html: body,
+    });
+  };
+}
